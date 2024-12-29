f2aab9646e4600e11bd6acbf7a81c233a02ae9e5
TuperWar2.0 <80649077+BillyTheSecond@users.noreply.github.com>
2024-12-29 22:05:45 +0100

    fix: Problems with the translated website and relative links in react fixed - Import of a new function  useBaseUrl to take into account the /en/ or any other future languages in the images, pages or icons links

--- INSAstuces/src/components/Button/index.js
+++ INSAstuces/src/components/Button/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Link from '@docusaurus/Link';
 import Style from './Button.module.css'; // Import the CSS file
 
+
 const Button = ({ textContent, to, size = "regular", style="default" }) => {
   return (
     <Link className={`${Style.button} ${Style[size]} ${Style[style]}`}
