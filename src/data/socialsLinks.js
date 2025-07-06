2c4873e06dc5da90c110a751398efcfe4bdfaa36
TuperWar2.0 <80649077+BillyTheSecond@users.noreply.github.com>
2025-07-06 22:30:11 +0200

    fix: Very small improvements - Passage des icones en svg - Responsive mais ça l'était déjà - Remove le fond transparent et les bords des icones

--- src/data/socialsLinks.js
+++ src/data/socialsLinks.js
@@ -1,32 +1,32 @@
 export const socialsLinks = [
   {
     title: "Instagram",
-    image: "/img/icons/instagram.jpg",
+    image: "/img/icons/socials/instagram.svg",
     link: "https://www.instagram.com/bdeinsahdf",
   },
   {
     title: "LinkedIn",
-    image: "/img/icons/linkedin.webp",
+    image: "/img/icons/socials/linkedin.svg",
     link: "https://linkedin.com/school/insa-hauts-de-france",
   },
   {
     title: "Discord",
-    image: "/img/icons/discord.webp",
+    image: "/img/icons/socials/discord.svg",
     link: "https://discord.com/invite/zcaAFwxKpQ",
   },
   {
     title: "Site web",
-    image: "/img/favicon.svg",
+    image: "/img/icons/socials/bde.svg",
     link: "https://bde.insahdf.fr/",
   },
   {
     title: "TikTok",
-    image: "/img/icons/tiktok.webp",
+    image: "/img/icons/socials/tiktok.svg",
     link: "https://www.tiktok.com/@bdeinsahdf",
   },
   {
     title: "Facebook",
-    image: "/img/icons/facebook.webp",
+    image: "/img/icons/socials/facebook.svg",
     link: "https://www.facebook.com/bde.insa.hdf/?locale=fr_FR",
   },
 ];
